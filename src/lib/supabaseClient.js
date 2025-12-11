1fca996d9369d7a73bf7c77877c4e92a07a33587
lauragrauer <lg845@drexel.edu>
2025-12-11 04:22:11 -0800

    a
    a

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,6 @@
-// src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://igweiigbzktxrbkplbps.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlnd2VpaWdiemt0eHJia3BsYnBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0MzA2MzQsImV4cCI6MjA4MTAwNjYzNH0.0XfZKSPbSfJdMIZhh2eYzUXu1LWGWWhfa6zFVQ5RtgU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
